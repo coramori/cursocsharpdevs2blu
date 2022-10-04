f3850913490c1dc5cb7a18c92d8c35726f64a4be
Cora Amorim <109560393+coramori@users.noreply.github.com>
2022-10-04 18:47:10 -0300

    Mergetest (#6)
    * [Aula OOP] Cadastro e Listagem de Pacientes
    
    * Merge master

---
+++ BancoDados/Aula1/SQL_INSERT_Usuario.sql
@@ -0,0 +1,13 @@
+INSERT INTO usuario(id_pessoa,
+					login,
+                    senha,
+                    status)
+VALUES (1, 'coramori', '123456', 'A'),
+	   (2,'alexandre', '123456', 'A'),
+	   (3,'anam', '123456', 'A'),
+       (4,'bernardom', '123456', 'A'),
+	   (5,'carlosx', '123456', 'A'),
+	   (6,'coraa', '123456', 'A'),
+	   (7,'eduardoc', '123456', 'A'),
+	   (8,'yasmina', '123456', 'A')
+;
